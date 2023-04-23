950127c6e1a83012e37a7fc4886a1d4f79388f9e
Chenglin Wei <chengliw@andrew.cmu.edu>
2023-04-23 16:15:22 -0700

    Add calculateWithoutMethod function

--- src/main/java/cmu/csdetector/metrics/calculators/type/LCOM3Calculator.java
+++ src/main/java/cmu/csdetector/metrics/calculators/type/LCOM3Calculator.java
@@ -1,8 +1,15 @@
 package cmu.csdetector.metrics.calculators.type;
 
+import cmu.csdetector.ast.visitors.ClassFieldAccessCollector;
+import cmu.csdetector.ast.visitors.MethodCollector;
 import cmu.csdetector.metrics.MetricName;
+import cmu.csdetector.resources.Method;
+import cmu.csdetector.resources.Resource;
 import org.eclipse.jdt.core.dom.ASTNode;
+import org.eclipse.jdt.core.dom.MethodDeclaration;
 import org.eclipse.jdt.core.dom.TypeDeclaration;
+
+import java.util.List;
 
 public class LCOM3Calculator extends LCOMCalculator {
 
@@ -11,13 +18,41 @@ public class LCOM3Calculator extends LCOMCalculator {
         TypeDeclaration type = (TypeDeclaration)target;
 
         this.calculateMetrics(type);
-        double lcom2 = (this.m - (this.sumMA / this.a)) / (this.m - 1);
-        return Double.isNaN(lcom2) ? 0 : lcom2;
+        double lcom3 = (this.m - (this.sumMA / this.a)) / (this.m - 1);
+        return Double.isNaN(lcom3) ? 0 : lcom3;
     }
 
     @Override
     public MetricName getMetricName() {
         return MetricName.LCOM3;
+    }
+
+    public Double calculateWithoutMethod(Resource the_class, Method the_method) {
+        TypeDeclaration type = (TypeDeclaration) the_class.getNode();
+
+        MethodCollector mCollector = new MethodCollector();
+        ClassFieldAccessCollector cfaCollector = new ClassFieldAccessCollector(type);
+
+        type.accept(mCollector);
+        List<MethodDeclaration> methods = mCollector.getNodesCollected();
+
+        double new_m = this.m - 1;
+        double new_a = this.a;
+        for (MethodDeclaration method : methods) {
+            if (method.equals(the_method.getNode())) {
+                continue;
+            }
+            cfaCollector.clearLocalReferences();
+            method.accept(cfaCollector);
+
+        }
+        double new_sumMA = cfaCollector.getNodesCollected().size();
+
+        double lcom3 = (new_m - (new_sumMA / new_a)) / (new_m - 1);
+        return Double.isNaN(lcom3) ? 0 : lcom3;
+    }
+    public Double calculateWithAdditionalMethod(Resource the_class, Method method) {
+        return 0.0;
     }
 
 }
--- src/main/java/cmu/csdetector/refactor/MethodMover.java
+++ src/main/java/cmu/csdetector/refactor/MethodMover.java
@@ -1,6 +1,7 @@
 package cmu.csdetector.refactor;
 
 import cmu.csdetector.metrics.MetricName;
+import cmu.csdetector.metrics.calculators.type.LCOM3Calculator;
 import cmu.csdetector.resources.Method;
 import cmu.csdetector.resources.Resource;
 import cmu.csdetector.resources.Type;
@@ -11,32 +12,26 @@ public class MethodMover {
     public Resource moveMethod(Method method, Resource source_class, ArrayList<Type> classes) {
         // returns the class the method should be moved to
         // Resource source_class = method.getBelongingClass();
+        LCOM3Calculator lcom3Calculator = new LCOM3Calculator();
         Double source_lcom3 = source_class.getMetricValue(MetricName.LCOM3);
+        System.out.println("old source lcom3: " + source_lcom3);
         for (Resource target_class: classes) {
-            Resource old_class = target_class;
-            Double old_class_lcom3 = old_class.getMetricValue(MetricName.LCOM3);
-            Resource new_class = simulateMove(method, target_class);
-            Double new_class_lcom3 = new_class.getMetricValue(MetricName.LCOM3);
-            Double new_source_lcom_3 = source_class.getMetricValue(MetricName.LCOM3);
-            if (new_class_lcom3 < old_class_lcom3 && new_source_lcom_3 < source_lcom3) {
-                return new_class;
+            Double old_target_lcom_3 = target_class.getMetricValue(MetricName.LCOM3);
+
+            Double new_source_lcom_3 = lcom3Calculator.calculateWithoutMethod(source_class, method);
+            Double new_target_lcom_3 = lcom3Calculator.calculateWithAdditionalMethod(target_class, method);
+            System.out.println("target class: " + target_class.getFullyQualifiedName());
+            System.out.println("new source lcom3: " + new_source_lcom_3);
+            System.out.println("old target lcom3: " + old_target_lcom_3);
+            System.out.println("new target lcom3: " + new_target_lcom_3);
+
+            if (new_target_lcom_3 < old_target_lcom_3 && new_source_lcom_3 < source_lcom3) {
+                return target_class;
             }
         }
         return null;
     }
 
-    public Resource simulateMove(Resource method, Resource target_class) {
-        // TODO: implement this method
-        return target_class;
-    }
-    public Resource getBelongingClass(Resource method) {
-        return null;
-    }
-    // 0. Imagine we have a method to move
-    // 1. get the class with the highest number of calls
-    // 2. move the method to that class
-    // 3. run feature envy detector again
-    // not for now but maybe for the future: if the method is still feature envy, move it to the next class
-    // not for now but maybe for the future: repeat until the method is not feature envy anymore
-    // return the name of the class to move the method
+
+
 }
