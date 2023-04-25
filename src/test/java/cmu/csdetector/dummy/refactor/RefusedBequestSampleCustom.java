c20a709561a48a59dfb24c847211de4b3a6ed3ce
YIHONG WANG <63112267+elizabteth-wang4477@users.noreply.github.com>
2023-04-25 13:18:25 -0700

    test refactor

---
+++ src/test/java/cmu/csdetector/dummy/refactor/RefusedBequestSampleCustom.java
@@ -0,0 +1,23 @@
+package cmu.csdetector.dummy.refactor;
+
+import cmu.csdetector.dummy.smells.SuperDummy;
+
+public class RefusedBequestSampleCustom extends SuperDummyCustom {
+
+	@Override
+	protected void a() {
+		super.a();
+	}
+
+	@Override
+	protected void b() {
+		super.b();
+	}
+
+	@Override
+	protected void c() {
+		super.c();
+	}
+
+	
+}
---
+++ src/test/java/cmu/csdetector/dummy/refactor/SuperDummyCustom.java
@@ -0,0 +1,25 @@
+package cmu.csdetector.dummy.refactor;
+
+public abstract class SuperDummyCustom {
+	protected String a;
+	
+	protected void a() {
+
+	}
+	
+	protected void b() {
+
+	}
+	
+	protected void c() {
+
+	}
+	
+	protected void d() {
+
+	}
+	
+	protected void e() {
+
+	}
+}
